7ccb3437ce03ff1288e59aa67d8bc74164843b9b
Tria Cahya Ramadhan <tria.ramadhan@aeonindonesia.co.id>
2024-09-23 21:09:51 +0700

    controller:add init insert and validate

--- routes/priceCheckerRoute.js
+++ routes/priceCheckerRoute.js
@@ -1,82 +1,81 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var jwt = require('jsonwebtoken');
-var bodyParser = require('body-parser');
-var jwt = require('../libs/jwt');
+var jwt = require("jsonwebtoken");
+var bodyParser = require("body-parser");
+var jwt = require("../libs/jwt");
 
-var priceCheckerController = require('../controllers/pc/priceCheckerController');
+var priceCheckerController = require("../controllers/pc/priceCheckerController");
 
-require('dotenv').config();
+require("dotenv").config();
 
-process.on('uncaughtException', function (err) {
-    console.log(err);
-  });
+process.on("uncaughtException", function (err) {
+  console.log(err);
+});
 
 const detectBearerToken = async (req, res, next) => {
-
-    const authHeader = req.headers['authorization'];
-    if (authHeader && authHeader.startsWith('Bearer ')) {
-
-        const token = authHeader.split(' ')[1];
-        if (!token) {
-          var output = {
-              "token": "Bearer token not found",
-              "status" : false,
-              "message": "Error",
-              "data": []
-          }
-
-          res.contentType('application/json').status(200);
-          var valued = JSON.stringify(output);
-          res.send(valued);
-        }
-    
-        const checkToken = await jwt.ConfigurationToken(token);
-
-        if(checkToken.status){
-          next();
-        }else{
-          var output = {
-              "token": "Bearer token not found",
-              "status" : false,
-              "message": "Error",
-              "data": []
-          }
-
-          res.contentType('application/json').status(200);
-          var valued = JSON.stringify(output);
-          res.send(valued);
-        } 
-
-    }else{
+  const authHeader = req.headers["authorization"];
+  if (authHeader && authHeader.startsWith("Bearer ")) {
+    const token = authHeader.split(" ")[1];
+    if (!token) {
       var output = {
-          "token": "Bearer token not found",
-          "status" : false,
-          "message": "Error",
-          "data": []
-      }
+        token: "Bearer token not found",
+        status: false,
+        message: "Error",
+        data: [],
+      };
 
-      res.contentType('application/json').status(200);
+      res.contentType("application/json").status(200);
       var valued = JSON.stringify(output);
       res.send(valued);
-    } 
+    }
+
+    const checkToken = await jwt.ConfigurationToken(token);
+
+    if (checkToken.status) {
+      next();
+    } else {
+      var output = {
+        token: "Bearer token not found",
+        status: false,
+        message: "Error",
+        data: [],
+      };
+
+      res.contentType("application/json").status(200);
+      var valued = JSON.stringify(output);
+      res.send(valued);
+    }
+  } else {
+    var output = {
+      token: "Bearer token not found",
+      status: false,
+      message: "Error",
+      data: [],
+    };
+
+    res.contentType("application/json").status(200);
+    var valued = JSON.stringify(output);
+    res.send(valued);
+  }
 };
 
-router.use(bodyParser.urlencoded({
-    extended: true
-}));
+router.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 router.use(bodyParser.json());
 //router.use(detectBearerToken);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'PT. AEON INDONESIA' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "PT. AEON INDONESIA" });
 });
 
-router.post('/check', priceCheckerController.check);
-// router.post('/insert', priceCheckerController.insertDevice);
-// router.post('/validate', priceCheckerController.validateDevice);
+// router.post('/check', priceCheckerController.check);
+router.post("/insert", priceCheckerController.insert);
+router.post("/validate", priceCheckerController.validate);
 // router.post('/validate-insert', priceCheckerController.validateAndInsertDevice);
 
 module.exports = router;
